90116c4c13b03725dcd55d272a40705e6daa358d
Nipuna Thathsara <98267228+nipunasamaraweera@users.noreply.github.com>
2023-08-01 14:34:57 +1000

    commit 3

--- server.js
+++ server.js
@@ -3,22 +3,42 @@ const res = require("express/lib/response");
 var app = express()
 var port = process.env.port || 3000;
 
-app.use(express.static(__dirname + '/'));
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.render('index.html')
+app.get('/add', (req, res) => {
+    const { num1, num2 } = req.query;
+    if (!num1 || !num2) {
+        return res.status(400).json({ error: 'Please provide both num1 and num2 parameters.' });
+    }
+    const result = parseFloat(num1) + parseFloat(num2);
+    return res.json({ result });
 });
 
-app.post('',(req,res)=>
-{});
-
-app.use('/addTwoNumbers', (req,res)=>{
-    var n1= req.query.n1;
-    var n2= req.query.n2;
-    var result= addNumbers(n1,n2);
-    res.json({statuscode:200,data:result,message:"Success"});
+app.post('/calculate', (req, res) => {
+    const { operation, num1, num2 } = req.body;
+    if (!operation || !num1 || !num2) {
+        return res.status(400).json({ error: 'Please provide operation, num1, and num2 parameters.' });
+    }
+    let result;
+    switch (operation) {
+        case 'add':
+            result = parseFloat(num1) + parseFloat(num2);
+            break;
+        case 'subtract':
+            result = parseFloat(num1) - parseFloat(num2);
+            break;
+        case 'multiply':
+            result = parseFloat(num1) * parseFloat(num2);
+            break;
+        case 'divide':
+            result = parseFloat(num1) / parseFloat(num2);
+            break;
+        default:
+            return res.status(400).json({ error: 'Invalid operation. Supported operations: add, subtract, multiply, divide.' });
+    }
+    return res.json({ result });
 });
 
-app.listen(port,()=>{
-console.log("App listening to: "+port)
-})
+app.listen(port, () => {
+    console.log(`App listening to http://localhost:${port}`);
+});
